59011643de824a860f4c640a4c093fd14337faaf
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2019-12-03 15:00:35 +0100

    Creates new vue project with vuex

--- wentworth/src/App.vue
+++ wentworth/src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
-    <router-view/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
