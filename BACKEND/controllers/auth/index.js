5a6d93eb252e74392c32495d72d3eb4e7d3e29ec
mezlet <esiaguchimezieleticia@rocketmail.com>
2019-12-04 18:09:15 +0100

    delete front end

--- BACKEND/controllers/auth/index.js
+++ BACKEND/controllers/auth/index.js
@@ -5,7 +5,6 @@ import db from '../../config/db';
 import * as Response from '../../utils/response';
 import auth from '../../utils/auth';
 import * as Queries from '../../utils/queries';
-import { password, aes_key, ivkey, organisation_code } from '../../app'
 
 export default class Authenticaion {
     static async Signup (req, res) {
@@ -30,57 +29,20 @@ export default class Authenticaion {
     }
 
     static async BVN (req, res) {
-        if(!req.headers['authorization']) Response.badrequestError(res, 'Please Enter an authorization token');
         try{
             const { bvn, dob } = req.body;
-            const BVN=bvn;
-            const DOB = dob;
-            const date = moment.utc().format('YYYYMMDD');
-            const username = process.env.SANDBOX_ORGANIZATION_CODE;
-            const signatureString = `${username}${date}${password}`;
-            const hash = crypto.createHash('sha256');
-            const signatureHeader = hash.update(signatureString).digest('hex');
-            const authString = `${username}:${password}`;
-            const authHeader = Buffer.from(authString).toString('base64');
-            const signatureMethodHeader = 'SHA256';
-
-            const bvn_obj = {BVN: bvn}
-            const encrypted = auth.encrypt(JSON.stringify(bvn_obj));
-            
-            const options={
-                method: 'POST',
-                uri: 'https://innovation-sandbox-backend.herokuapp.com/nibss/bvnr/VerifySingleBVN',
-                headers: {
-                "Sandbox-Key": process.env.SANDBOX_KEY,
-                "OrganisationCode": organisation_code,
-                "Authorization": authHeader,
-                "SIGNATURE": signatureHeader,
-                "SIGNATURE_METH": signatureMethodHeader,
-                "Accept": 'application/json',
-                "Content-Type": 'application/json',
-                },
-                body: encrypted            
-            }
-
-
+            const options = auth.BVNData(bvn);
+            const { firstname, lastname, phone_number} = req.user;
     await rp.post(options, async(err, response, body)=>{
             const result = auth.decrypt(body);
             if(JSON.parse(result).message){
-                const token = req.headers['authorization'];
-                const decoded = auth.decodeToken(token);
                 const { PhoneNumber, FirstName, LastName } = JSON.parse(result).data;
-                console.log(JSON.parse(result).data);
-                const user = await db(Queries.getUser, [decoded.userId]);
-                if(user && !user.rows.length > 0){
-                    return res.status(404).json({'success': false, 'error': 'Error verifying user.'});
-                }
-                const { firstname, lastname, phone_number } = user.rows[0];
                 if(firstname === FirstName && lastname === LastName && phone_number === PhoneNumber ){       
-                    const update = await db(Queries.updateUser, [PhoneNumber, BVN, DOB]);
+                    const update = await db(Queries.updateUser, [PhoneNumber, bvn, dob]);
                     return (update&&update.rows[0]) ? Response.successResponse(res, 'BVN Matched.')
                     : Response.serverError(res, 'Error updating user data');
                 }
-                return res.status(400).json({'success': false, 'error': 'BVN Mismatch'});
+                return res.status(400).json({'success': false, 'message': 'BVN Mismatch'});
             }
             return Response.badrequestError(res, 'Invalid BVN');
         })    
