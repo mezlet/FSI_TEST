e24a767d2d004d637d8e9d002e11d9e3d624acbd
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2019-12-04 12:36:20 +0100

    Adds vuelidate

--- FRONTEND/src/main.js
+++ FRONTEND/src/main.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import Vuelidate from "vuelidate";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
+Vue.use(Vuelidate);
 Vue.config.productionTip = false;
 
 new Vue({
