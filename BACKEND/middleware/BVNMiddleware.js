a606ace9aed2ca7e6ed831a23337345e3910a6d8
mezlet <esiaguchimezieleticia@rocketmail.com>
2019-12-04 18:09:15 +0100

    delete front end

---
+++ BACKEND/middleware/BVNMiddleware.js
@@ -0,0 +1,33 @@
+/* eslint-disable consistent-return */
+import * as Response from '../utils/response';
+import db from '../config/db';
+import auth from '../utils/auth';
+import * as Queries from '../utils/queries';
+
+export default class BVNMiddleware {
+
+  static async checkToken (req, res, next) {
+if(!req.headers['authorization']) Response.badrequestError(res, 'Please provide a token');
+    try {
+      const token = req.headers['authorization'];
+      const decoded = auth.decodeToken(token);
+      if(!decoded){
+        return Response.serverError(res, 'Error decoding token')
+      }
+      const user = await db(Queries.getUser, [decoded.userId]);
+      if(!user && !user.rows > 0){
+        return Response.badrequestError(res, 'Invalid token')
+      }
+      delete user.rows[0].password;
+      req.user = {
+        ...user.rows[0]
+      }
+
+     return  next();
+
+    } catch (error) {
+      return Response.serverError(res, error.message || 'Something went wrong.');
+    }
+  }
+
+}
